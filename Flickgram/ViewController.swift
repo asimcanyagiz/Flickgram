b6414b125a846ab9fd0c30c599db279df1303b27
Asim Can Yagiz <meycasim@gmail.com>
2022-10-13 22:33:00 +0300

    Initial Commit

---
+++ Flickgram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flickgram
+//
+//  Created by Asım can Yağız on 13.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
